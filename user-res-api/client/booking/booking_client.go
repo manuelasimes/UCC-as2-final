3b021043a29e43ff1ede8f6ac0f781b9cf7bab73
manuelasimes <manuela.simes10@gmail.com>
2023-09-29 10:49:37 -0300

    Solo hice el service y cosas para el user

--- user-res-api/client/booking/booking_client.go
+++ user-res-api/client/booking/booking_client.go
@@ -2,3 +2,4 @@
 //  El memcache tiene como parámetro el TTL (cantidad de tiempo que guardo info en caché). 
 // Conectamos con Amadeus que nos permite confirmar reserva en hotel usando un proveedor externo. 
 // Amadeus solo para reserva → válida la API la disponibilidad.
+// hay q hacer este... 
