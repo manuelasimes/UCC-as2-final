0e6ac90905740befa39348e41a26aa4b67f757d3
SantiagoP <santiagoplanasm@gmail.com>
2023-11-04 02:32:10 -0300

    ya todo hoteles api

--- hotels-api/router/router.go
+++ hotels-api/router/router.go
@@ -10,7 +10,7 @@ func MapUrls(router *gin.Engine) {
 	// Products Mapping
 	router.GET("/hotels/:id", hotelController.Get)
 	router.POST("/hotels", hotelController.Insert)
-	//router.PUT("/hotels/:id", hotelController.Update)
+	router.PUT("/hotels/:id", hotelController.Update)
 
 	fmt.Println("Finishing mappings configurations")
 }
