3f44a0908333aedcfbdc2abae90d3415e4d554a8
manuelasimes <manuela.simes10@gmail.com>
2023-11-04 18:55:34 -0300

    Conexion empezada con user-res-api

--- frontend/client/src/pages/login/login_cliente.jsx
+++ frontend/client/src/pages/login/login_cliente.jsx
@@ -3,16 +3,44 @@ import { AuthContext } from './auth';
 import { Link } from 'react-router-dom';
 import '../estilo/login_cliente.css';
 
+
+// probamos del front del sem pasado 
+async function login(username, password) {
+  return await fetch('http://localhost:8090/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({"username": username, "password": password})
+  })
+      .then(response => {
+        if (response.status == 400 || response.status == 401 || response.status == 403)
+        {
+          return {"user_id": -1, "user_type": "false"}
+        }
+        return response.json()
+      })
+      .then(response => {
+        Cookie.set("user_id", response.user_id, {path: '/'})
+        Cookie.set("username", username, {path: '/login'})
+        Cookie.set("user_type", response.type, {path: '/'})
+      })
+}
+
+
+
+
+
 const ClienteLogin = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { loginCliente } = useContext(AuthContext);
 
   const handleLoginCliente = () => {
-    fetch(`http://localhost:8090/cliente/email/${email}`)
+    fetch('http://localhost:8070/login')
     .then(response => response.json())
     .then(data => {
-      if (email === data.email && password === data.password) {
+      if (email === data.username && password === data.password) {
         const token = 'TOKEN_CLIENTE';
         loginCliente(token, data.id);
         window.location.href = '/';
